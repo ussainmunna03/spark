5541d50c52b7c64f1101e63bb4d4246cd2f84503
ussainmunna03 <90029972+ussainmunna03@users.noreply.github.com>
2021-09-03 19:46:15 +0530

    sample

---
+++ helloworld.py
@@ -0,0 +1,17 @@
+print("hello world !")
+a = 23
+for i in range(a):
+    print(i)
+
+m =[12,4,5,6]
+for j in m:
+    print(j)
+
+b = set(m)
+print(b)
+
+dict = {"name": [{"name1": "ussain", "name2": "tabas", "name3": "basha"}],
+        "age": "22",
+        "sex": "male"
+        }
+print(dict['name'][0]['name2'])
